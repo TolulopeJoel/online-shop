ab9c1917d99c23679da377334200e4df0f77a32c
Tolu Joel <toluisjoel@gmail.com>
2022-09-16 13:48:31 +0100

    cart views and urls

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from .cart import Cart
+from shop.models import Product
+from .forms import CartAddProductForm
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], overide_quantity=cd['overide'])
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
