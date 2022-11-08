304257856854c8634064da5fd928b688e67cd228
Tolu Joel <toluisjoel@gmail.com>
2022-11-08 12:26:06 +0100

    Implemented the order system

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,27 @@
+from itertools import product
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import OrderCreateForm
+from .models import OrderItem
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    form = OrderCreateForm(request.POST)
+    
+    if form.is_valid():
+        order = form.save()
+        for item in cart:
+            OrderItem.objects.create(
+                order=order,
+                product=item['product'],
+                price=item['price'],
+                quantity=item['quantity'],
+            )
+        cart.clear()
+        
+        return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
